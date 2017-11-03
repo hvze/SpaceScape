a6d8f7b8b234caab0f7a1ec9d2fbfae0773ef49d
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-03 11:45:09 -0400

    vffe

---
+++ SpaceEscape/src/yonathaWins/TylerCave.java
@@ -0,0 +1,30 @@
+package yonathaWins;
+
+import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
+
+public class TylerCave extends CaveRoom {
+
+	public TylerCave(String description) {
+		super(description);
+		boolean isJeff = false;
+	}
+
+	public String validKeys() {
+		return "wdsal";
+	}
+	
+	public void printAllowedEntry() {
+		CaveExplorer.print("You can only enter 'w', 'a', 's', or 'd' to move or"
+				+ "if you type l you can talk");}
+	public void performAction(int direction) {
+		CaveExplorer.print("Hello, tell me your name");
+		String userString = CaveExplorer.in.nextLine();
+		if(userString.equalsIgnoreCase("jeff") || userString.equalsIgnoreCase("doge") ) {
+			CaveExplorer.print("true");
+		}
+	}
+	
+
+
+}
