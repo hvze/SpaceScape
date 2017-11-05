db09a435e8a612546368e5a3eba10f4adf629c67
GarrettC <GarrettC@GarrettC-PC>
2017-11-05 14:54:17 -0500

    Finished GarrettRoom

--- SpaceEscape/src/rickyAndGarrett/GarrettRoom.java
+++ SpaceEscape/src/rickyAndGarrett/GarrettRoom.java
@@ -7,6 +7,8 @@ import caveExplorer.NPCRoom;
 public class GarrettRoom extends NPCRoom {
 	
 	private NPC presentNPC;
+	private String[] trivia = {"Taco Bell was created in 1962 - nearly 55 years ago!","In Japan, letting a sumo wrestler make your baby cry is considered good luck.",
+			"Between 1900 and 1920, Tug of War was an Olympic event.","The Code of Hammurabi decreed that bartenders who watered down beer would be executed."};
 
 	public GarrettRoom(String description) {
 		super(description);
@@ -26,8 +28,13 @@ public class GarrettRoom extends NPCRoom {
 	}
 
 	public void interact() {
-		// TODO Auto-generated method stub
-		
+		CaveExplorer.print("Welcome to the Trivia Room. Enter 'trivia' for a random fact");
+		String s = CaveExplorer.in.nextLine();
+		while(!s.equalsIgnoreCase("trivia")) {
+			CaveExplorer.print("Please type 'trivia' for knowledge and maybe a chuckle.");
+			s = CaveExplorer.in.nextLine();
+		}
+		CaveExplorer.print(trivia[(int)(Math.random() * trivia.length)]);
 	}
 	
 
