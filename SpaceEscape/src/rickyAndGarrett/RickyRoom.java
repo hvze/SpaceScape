846874dbb85d5bec4e57e354f804e02cc1e3b5ce
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-11-06 10:32:18 -0500

    Changed the name of my npc room

--- SpaceEscape/src/rickyAndGarrett/RickyRoom.java
+++ SpaceEscape/src/rickyAndGarrett/RickyRoom.java
@@ -16,7 +16,7 @@ public class RickyRoom extends NPCRoom {
 	public void performAction(int direction) {
 		if(direction == 4) {
 			if(containsNPC() && presentNPC.isActive()) {
-				this.interact();
+				interact();
 			}
 			else {
 				CaveExplorer.print("There's no one here who is active to talk to you...");
@@ -28,15 +28,17 @@ public class RickyRoom extends NPCRoom {
 	}
 	
 	public void interact() {
-		CaveExplorer.print("Welcome to Ricky's NPC Room. Here, You will be entered for a quick break from your journey");
+		CaveExplorer.print("Welcome to the Break Room. Here, You will be entered for a quick break from your journey");
 		String s = CaveExplorer.in.nextLine();
 		while(!(s.equalsIgnoreCase("leave"))){
-			CaveExplorer.print("I am waiting for an answer, I know I'm an NPC and all I do is talk, but I'm getting bored.('psst! Type 'leave' in order to leave this room.'");
+			CaveExplorer.print("I am waiting for an answer, I know I'm an NPC and all I do is talk, but I'm getting bored.('psst! Type 'leave' in order to leave this room.')");
 			s = CaveExplorer.in.nextLine();
 		}
 		CaveExplorer.print("Well, It was nice meeting you. Good Luck on your jorney and safe travels.");
 	}
 	
 }
-//CaveRoom customRoom = new NPCRoom("Room);
-//CaveExplorer.caves[2][3] = customRoom
+/*
+	CaveRoom customRoom = new NPCRoom("Room");
+	CaveExplorer.caves[2][3] = customRoom;
+*/	
