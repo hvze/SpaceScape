220d28115041b0fd9750809b1ac144aaa20348d8
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-11-14 11:28:43 -0500

    edit

--- SpaceEscape/src/fahadAndDavid/FahadFrontend.java
+++ SpaceEscape/src/fahadAndDavid/FahadFrontend.java
@@ -9,8 +9,23 @@ public class FahadFrontend implements DavidSupport{
 		demo.play();
 	}
 	
-	private void play() {
-		// TODO Auto-generated method stub
+	private void play() {public void play(){
+	    while(backend.stillPlaying()){
+	        displayBoard();
+	        displayScore();
+	        String input = backend.getValidUserInput();
+	        respondToInput(input);
+
+	        backend.computerMove();//this backend method might be where the backend partner 
+	            //programs everything that would earn most credit for 2D arrays
+
+	        analyzeBoard();//this frontend method might be where the frontend partner 
+	            //programs everything that would earn most credit for 2D arrays
+
+	        updateScore();
+	    }
+	        printGameOverMessage(backend.victorious());
+	}
 		
 	}
 
