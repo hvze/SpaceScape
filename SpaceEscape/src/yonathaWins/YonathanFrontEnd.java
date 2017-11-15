644c6fe3187dcb3571fe7990c8315d024a12d982
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-11-15 11:45:04 -0500

    murp

--- SpaceEscape/src/yonathaWins/YonathanFrontEnd.java
+++ SpaceEscape/src/yonathaWins/YonathanFrontEnd.java
@@ -7,7 +7,11 @@ public class YonathanFrontEnd implements TylerSupport {
 	//randomly populate with goalfiller
 	//before a square is populated, check if it's already populated
 	//randomly check when a thing is populate whether or not it's a starter and if it's a starter than make an end goal randomly around the map
+	private String identifier;
 	
+	//match order to tylers indexs in setorientation
+	public String orientation[]= {"___\n|"+identifier+" \n|  ","___\n "+identifier
+			};
 	public static CaveRoom[][] flowRoom;
 	private YonathanSupport frontend;
 	private int linesComplete;
